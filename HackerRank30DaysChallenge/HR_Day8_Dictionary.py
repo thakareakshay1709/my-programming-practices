c0a55170be728839c06d2ff606f42c1147471a4a
Akshay Thakare <thakare.akshay1709@gmail.com>
2020-05-04 19:00:30 +0100

    HR 8/30 days updated solution for dictionary

--- HackerRank30DaysChallenge/HR_Day8_Dictionary.py
+++ HackerRank30DaysChallenge/HR_Day8_Dictionary.py
@@ -1,23 +1,38 @@
 
 #Creating empty dictionary
 
-phoneBook = {}
+# phoneBook = {}
+#
+# totalEntries = int(input("Enter total entries\n"))
+#
+#
+# for _ in range(0, totalEntries):
+# 	user, telephone = input().split()   ##split function in python uses to accept multiple input from user with some seperator
+# 	phoneBook[user] = telephone
+#
+# for _ in range(0, totalEntries):
+# 	user = input()
+# 	if user in phoneBook:
+#
+# 		print(user + "=" + phoneBook[user])
+# 	else:
+# 		print("Not found")
 
-totalEntries = int(input("Enter total entries\n"))
+import sys
+
+n = int(input())
+phone_book = {}
+
+for _ in range(n):
+	element = list(map(str, input().rstrip().split()))
+	phone_book[element[0]] =(element[1])
+#print(phone_book)
 
 
-for _ in range(0, totalEntries):
-	user, telephone = input().split()   ##split function in python uses to accept multiple input from user with some seperator
-	phoneBook[user] = telephone
-
-for _ in range(0, totalEntries):
-	user = input()
-	if user in phoneBook:
-		print("{}={}".format(user, phoneBook[user]))
+s = input()
+while(s):
+	if phone_book.__contains__(s):
+		print (s+"="+phone_book[s])
 	else:
-		print("Not found")
-
-
-
-
-#print(phoneBook)
+		print('Not found')
+	s = sys.stdin.readline().strip()
