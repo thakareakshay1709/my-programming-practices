42ada0a2a6c81d0c75806ca44c08ae31ade92428
Akshay Thakare <thakare.akshay1709@gmail.com>
2020-05-12 17:03:04 +0100

    HR 22/30 days python solution for height of BST

---
+++ HackerRank30DaysChallenge/HR_Day22_BST.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def getHeight(self,root):
+        #Write your code here
+        if root:
+            leftHeight = 0
+            rightHeight = 0
+            if(root.left):
+                leftHeight = self.getHeight(root.left) + 1
+            if(root.right):
+                rightHeight = self.getHeight(root.right) + 1
+            return max(leftHeight,rightHeight)
+        else:
+            return 0
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+height=myTree.getHeight(root)
+print(height)
