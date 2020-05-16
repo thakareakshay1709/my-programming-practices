e95473393078e275ca987d75a9da848df362e954
Akshay Thakare <thakare.akshay1709@gmail.com>
2020-05-16 14:22:31 +0100

    HR 26/30 days python solution for nested logic

---
+++ HackerRank30DaysChallenge/HR_Day26_NestedLogic.py
@@ -0,0 +1,35 @@
+#Accepting user inptus
+
+ret_date,ret_mon,ret_year = input().split()
+due_date,due_mon,due_year = input().split()
+
+#print("Retured Dates",ret_date,ret_mon,ret_year)
+#print("Due Dates",due_date,due_mon,due_year)
+
+def calculateFine(ret_date, ret_mon, ret_year, due_date, due_mon, due_year):
+    daily_fine = 15
+    monthly_fine = 500
+    yearly_fine = 10000
+
+
+    if int(ret_date) == int(due_date) and int(ret_mon) == int(due_mon) and int(ret_year) == int(due_year):
+        return 0
+    elif ret_date != due_date and ret_mon == due_mon and ret_year == due_year:
+        if(int(ret_date) < int(due_date)):
+            return 0
+        else:
+            return daily_fine * (int(ret_date) - int(due_date))
+    elif ret_mon != due_mon and ret_year == due_year:
+        if (int(ret_mon) < int(due_mon)):
+            return 0
+        else:
+            return monthly_fine * (int(ret_mon) - int(due_mon))
+    elif ret_year != due_year:
+        if(int(ret_year) < int(due_year)):
+            return 0
+        else:
+            return yearly_fine
+
+
+fine = calculateFine(ret_date,ret_mon,ret_year,due_date,due_mon,due_year)
+print(fine)
