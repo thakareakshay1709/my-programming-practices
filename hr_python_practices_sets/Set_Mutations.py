a5cc81dc3c4aa6cf0da883fed44e0f225b0270df
Akshay Thakare <thakare.akshay1709@gmail.com>
2021-02-01 23:47:25 +0000

    Set mutations

---
+++ hr_python_practices_sets/Set_Mutations.py
@@ -0,0 +1,16 @@
+length=int(input())
+s=set(map(int, input().split()))
+N=int(input())
+
+for i in range(N):
+    (p, q)=input().split()
+    s2=set(map(int, input().split()))
+    if p=='intersection_update':
+        s.intersection_update(s2)
+    elif p=='update':
+        s.update(s2)
+    elif p=='symmetric_difference_update':
+        s.symmetric_difference_update(s2)
+    elif p=='difference_update':
+        s.difference_update(s2)
+print(sum(s))
