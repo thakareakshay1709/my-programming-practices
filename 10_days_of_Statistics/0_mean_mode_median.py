87fc97969b239c3255be7179cd6ea24e52e6df46
Akshay Thakare <thakare.akshay1709@gmail.com>
2021-02-05 00:38:05 +0000

    hackerrank stats day 0 mean,median,mode

---
+++ 10_days_of_Statistics/0_mean_mode_median.py
@@ -0,0 +1,17 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+# import statistics
+# from
+# no = input()
+# array = sorted(list(map(int, input().split())))
+
+# print(statistics.mean(array))
+# print(statistics.median(array))
+# print(statistics.mode(array))
+import numpy as np
+from scipy import stats
+
+size = int(input())
+numbers = list(map(int, input().split()))
+print(np.mean(numbers))
+print(np.median(numbers))
+print(int(stats.mode(numbers)[0]))
