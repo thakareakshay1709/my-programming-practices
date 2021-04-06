e743670463956c5956e1c5cbe837c033dee0eb84
Akshay Thakare <57269333+thakareakshay1709@users.noreply.github.com>
2021-04-06 16:56:43 +0100

    hackerrank python solution for validating roman numerals

---
+++ hr_python_practices_regex_parsing/validating_roman_numerals.py
@@ -0,0 +1,10 @@
+import re
+
+thousand = "(?:(M){0,3})?"
+hundred = "(?:(D?(C){0,3})|(CM)|(CD))?"
+ten = "(?:(L?(X){0,3})|(XC)|(XL))?"
+unit = "(?:(V?(I){0,3})|(IX)|(IV))?"
+
+regex_pattern = r"^" + thousand + hundred + ten + unit + "$"
+
+print(str(bool(re.match(regex_pattern, input()))))
